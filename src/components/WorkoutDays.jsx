479e3534df6adb6a7e36d3b17dd23b08222ef7a9
Kevin Nielsen <keveran@gmail.com>
2023-02-26 11:09:59 +0100

    added workoutDays functionality

--- src/components/WorkoutDays.jsx
+++ src/components/WorkoutDays.jsx
@@ -4,40 +4,48 @@ export default function WorkoutDays() {
   const today = new Date().toDateString();
   // console.log(today.split(' ')[0])
 
-  const mon = useRef(null)
-  const tue = useRef(null)
-  const wed = useRef(null)
-  const thu = useRef(null)
-  const fri = useRef(null)
-  const sat = useRef(null)
-  const sun = useRef(null)
+  const MON = useRef(null)
+  const TUE = useRef(null)
+  const WED = useRef(null)
+  const THU = useRef(null)
+  const FRI = useRef(null)
+  const SAT = useRef(null)
+  const SUN = useRef(null)
 
-  const days = [mon, tue, wed, thu, fri, sat, sun];
-  const workedOut = [1, 3, 4];
+  const days = [MON, TUE, WED, THU, FRI, SAT, SUN];
+  const workedOut = ['TUE', 'THU', 'FRI'];
+
+  useEffect(() => {
+    for(let i = 0; i < days.length; i++) {
+      if(workedOut.includes(days[i].current.childNodes[0].innerHTML)) {
+        days[i].current.classList.add('bg-main')
+      };
+    }
+  }, [])
 
   return(
     <section className='mb-2'>
       <h2>Workouts this week</h2>
       <div className='pt-2 pb-6 bg-light rounded-main flex justify-evenly items-center'>
-        <div ref={mon} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
+        <div ref={MON} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
           <p className='absolute mt-14'>MON</p>
         </div>
-        <div ref={tue} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
+        <div ref={TUE} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
           <p className='absolute mt-14'>TUE</p>
         </div>
-        <div ref={wed} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
+        <div ref={WED} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
           <p className='absolute mt-14'>WED</p>
         </div>
-        <div ref={thu} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
+        <div ref={THU} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
           <p className='absolute mt-14'>THU</p>
         </div>
-        <div ref={fri} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
+        <div ref={FRI} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
           <p className='absolute mt-14'>FRI</p>
         </div>
-        <div ref={sat} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
+        <div ref={SAT} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
           <p className='absolute mt-14'>SAT</p>
         </div>
-        <div ref={sun} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
+        <div ref={SUN} className={`flex justify-center items-center rounded-full bg-gray-300 w-9 h-9`}>
           <p className='absolute mt-14'>SUN</p>
         </div>  
       </div>
